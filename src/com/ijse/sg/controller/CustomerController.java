8400670c01cd5465aaedb7574de6352b6c69684e
kavinduakash <bkkasamarasingha1220@gmail.com>
2026-05-07 21:23:09 +0530

    interact with bo

--- src/com/ijse/sg/controller/CustomerController.java
+++ src/com/ijse/sg/controller/CustomerController.java
@@ -4,6 +4,7 @@
  */
 package com.ijse.sg.controller;
 
+import com.ijse.sg.bo.CustomerBOImpl;
 import com.ijse.sg.dao.custom.CustomerDAO;
 import com.ijse.sg.dao.custom.impl.CustomerDAOImpl;
 import com.ijse.sg.dao.DAOFactory;
@@ -24,57 +25,33 @@ import java.util.List;
  */
 public class CustomerController {
    
-    private final CustomerDAO customerDAO = (CustomerDAO)DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.CUSTOMER);
+    CustomerBOImpl customerBO = new CustomerBOImpl();
     
     public boolean saveCustomer(CustomerDTO dto) throws Exception {
         
-            CustomerEntity customerEntity = convertCustomerDTOToCustomerEntity(dto);
-            boolean result = customerDAO.save(customerEntity);
+            boolean result = customerBO.saveCustomer(dto);
             return result;
     
     }
     
     public boolean updateCustomer(CustomerDTO dto) throws Exception {
             
-            CustomerEntity customerEntity = convertCustomerDTOToCustomerEntity(dto);
-            boolean result = customerDAO.update(customerEntity);
+            boolean result = customerBO.updateCustomer(dto);
             return result;
             
     }
     
     public boolean deleteCustomer(int id) throws Exception {
         
-        boolean result = customerDAO.delete(id);
-        return result;
+         boolean result = customerBO.deleteCustomer(id);
+         return result;
         
     }
     
     public List<CustomerDTO> getCustomers() throws Exception {
                
-        List<CustomerEntity> customers = customerDAO.getAll();
-        
-        List<CustomerDTO> dtos = new ArrayList<>();
-        
-        for (CustomerEntity customer : customers) {
-            CustomerDTO dto = convertCustomerEntityToCustomerDTO(customer);
-            dtos.add(dto);
-        }
-        
-        return dtos;
-        
-    }
-    
-    private CustomerEntity convertCustomerDTOToCustomerEntity(CustomerDTO dto) {
-        
-        CustomerEntity customerEntity = new CustomerEntity(dto.getId(), dto.getName(), dto.getEmail(), dto.getAddress());
-        return customerEntity;
-        
-    }
-    
-    private CustomerDTO convertCustomerEntityToCustomerDTO(CustomerEntity entity) {
-        
-        CustomerDTO customerEntity = new CustomerDTO(entity.getId(), entity.getName(), entity.getEmail(), entity.getAddress());
-        return customerEntity;
+       List<CustomerDTO> customerList = customerBO.getAllCustomers();
+       return customerList;
         
     }
     
