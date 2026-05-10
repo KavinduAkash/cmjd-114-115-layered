118d953980a815357f85f8000c355d587441157e
kavinduakash <bkkasamarasingha1220@gmail.com>
2026-05-10 08:55:35 +0530

    add OrderEntity

--- src/com/ijse/sg/entity/OrderEntity.java
+++ src/com/ijse/sg/entity/OrderEntity.java
@@ -11,7 +11,45 @@ import java.util.Date;
  * @author kavinduakash
  */
 public class OrderEntity {
-    int id;
-    int customerId;
-    Date date;
+    private int id;
+    private int customerId;
+    private Date date;
+
+    public OrderEntity() {
+    }
+
+    public OrderEntity(int id, int customerId, Date date) {
+        this.id = id;
+        this.customerId = customerId;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderEntity{" + "id=" + id + ", customerId=" + customerId + ", date=" + date + '}';
+    }
 }
